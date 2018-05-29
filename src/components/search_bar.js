f8a9ed8b9917b0b773e7aaf021bfbaca95e2ead9
JingaFoo <calvinmwariama@yahoo.com>
2018-05-29 12:50:32 -0400

    First API Call

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -3,10 +3,20 @@ import React, { Component } from 'react';
 import './search_bar.css';
 
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: ''};
+  }
+
   render() {
     return (
       <div className="input-group">
-        <input className="search-input form-control" />
+        <input
+          className="search-input form-control"
+          value={this.state.term}
+          onChange={e => this.setState({ term: e.target.value })}
+        />
         <div className="input-group-append">
           <button className="btn btn-outline-primary" type="button">
           <i className="icon ion-md-search"></i>
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import Navbar from './components/navbar';
-
 const API_KEY = 'AIzaSyD-Z7Dq-QyimQ1lC1JPixfMDmTA_y313as';
+
+YTSearch({ key: API_KEY, term: 'surfboards' }, function(data) {
+  console.log(data);
+});
 
 const App = () => {
   return (
