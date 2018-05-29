fde134314676c6b54914699585ce6c32ea7407fd
JingaFoo <calvinmwariama@yahoo.com>
2018-05-29 14:21:23 -0400

    Render video list and details

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -7,7 +7,7 @@ const VideoList = (props) => {
   });
   return (
     <div className="container">
-      <ul className="col-md-4 list-group">
+      <ul className="col-md-4 ml-auto">
         {videoItems}
       </ul>
     </div>
--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import './video_list_item.css';
 
-const VideoListItem = (props) => {
-  return <li className="list-group-item">Video</li>;
+const VideoListItem = ({video}) => {
+  console.log(video.snippet)
+  return (
+    <div className="video-item">
+      <div className="row no-gutters">
+        <div className="col-6">
+          <div className="video-thumbnail">
+            <img src={video.snippet.thumbnails.high.url} alt="Video Thumbnail"/>
+          </div>
+        </div>
+        <div className="col-6">
+          <a className="video-details" href="/">
+            <h3 className="video-title">{video.snippet.title}</h3>
+            <div className="channel-details">
+              <span>{video.snippet.channelTitle}</span>
+            </div>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default VideoListItem;
