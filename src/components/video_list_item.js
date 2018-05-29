b6bb56992acb0fa4b86985b4b08e6df1c363c66f
JingaFoo <calvinmwariama@yahoo.com>
2018-05-29 13:25:14 -0400

    Render list of videos

---
+++ src/components/video_list_item.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const VideoListItem = (props) => {
+  return <li className="list-group-item">Video</li>;
+}
+
+export default VideoListItem;
--- src/index.js
+++ src/index.js
@@ -2,17 +2,25 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import Navbar from './components/navbar';
+import VideoList from './components/video_list';
 import { API_KEY } from './config/keys';
 
-YTSearch({ key: API_KEY, term: 'surfboards' }, function(data) {
-  console.log(data);
-});
-
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] }
+
+    //Fetch initial videos from YouTube API with the search term
+    YTSearch({ key: API_KEY, term: 'tesla' }, (videos) => {
+      this.setState({ videos });
+    });
+  }
   render() {
     return (
       <div>
         <Navbar />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   };
