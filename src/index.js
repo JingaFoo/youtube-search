3f79e142411bdf8b9a596620b23bd7ed8722ccb5
JingaFoo <calvinmwariama@yahoo.com>
2018-05-29 13:01:52 -0400

    Refactor App to class component

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import Navbar from './components/navbar';
-const API_KEY = 'AIzaSyD-Z7Dq-QyimQ1lC1JPixfMDmTA_y313as';
+import { API_KEY } from './config/keys';
 
 YTSearch({ key: API_KEY, term: 'surfboards' }, function(data) {
   console.log(data);
 });
 
-const App = () => {
-  return (
-    <div>
-      <Navbar />
-    </div>
-  );
-};
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar />
+      </div>
+    );
+  };
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
