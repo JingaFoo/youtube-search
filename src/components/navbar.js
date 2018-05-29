2727a34a4a69c81d8c2966b9edc5823ad909c201
Foojinga <calvinmwariama@gmail.com>
2018-05-29 11:53:31 -0400

    Navbar customized

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -9,19 +9,28 @@ const Navbar = () => {
       <div className="container">
         <a className="navbar-brand" href="/">YouTube</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
+          <i className="icon ion-md-menu menu-icon"></i>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
-        <div className="nav-left col-3">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <a className="nav-link" href="/">Home</a>
-            </li>
-          </ul>
-        </div>
-        <div className="nav-right col-7">
-          <SearchBar />
-        </div>
+          <div className="nav-left col-3 d-none d-sm-block">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link" href="/">Home</a>
+              </li>
+            </ul>
+          </div>
+          <div className="nav-right col-7 d-none d-sm-block">
+            <SearchBar />
+          </div>
+          <div className="mobile-view d-block d-sm-none">
+            <ul className="navbar-nav">
+              <li className="nav-item ml-auto">
+                <a className="nav-link" href="/">Home</a>
+                <a className="nav-link" href="/">Channels</a>
+              </li>
+            </ul>
+            <SearchBar />
+          </div>
         </div>
       </div>
     </nav>
--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,18 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './search_bar.css';
 
-const SearchBar = () => {
-  return (
-    <div className="input-group">
-      <input className="search-input form-control" />
-      <div className="input-group-append">
-        <button className="btn btn-outline-secondary" type="button">
-        <i className="icon ion-md-search"></i>
-        </button>
+class SearchBar extends Component {
+  render() {
+    return (
+      <div className="input-group">
+        <input className="search-input form-control" />
+        <div className="input-group-append">
+          <button className="btn btn-outline-secondary" type="button">
+          <i className="icon ion-md-search"></i>
+          </button>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  };
+}
 
 export default SearchBar;
