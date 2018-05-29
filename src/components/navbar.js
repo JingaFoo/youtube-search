96c8f7f7bad592595c47fa7079a414596770de2e
JingaFoo <calvinmwariama@yahoo.com>
2018-05-29 12:08:02 -0400

    Refactor Navbar to class component

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,40 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import SearchBar from './search_bar';
 
 import './navbar.css';
 
-const Navbar = () => {
-  return (
-    <nav className="navbar navbar-expand-sm">
-      <div className="container">
-        <a className="navbar-brand" href="/">YouTube</a>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <i className="icon ion-md-menu menu-icon"></i>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <div className="nav-left col-3 d-none d-sm-block">
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <a className="nav-link" href="/">Home</a>
-              </li>
-            </ul>
-          </div>
-          <div className="nav-right col-7 d-none d-sm-block">
-            <SearchBar />
-          </div>
-          <div className="mobile-view d-block d-sm-none">
-            <ul className="navbar-nav">
-              <li className="nav-item ml-auto">
-                <a className="nav-link" href="/">Home</a>
-                <a className="nav-link" href="/">Channels</a>
-              </li>
-            </ul>
-            <SearchBar />
+class Navbar extends Component {
+  render() {
+    return (
+      <nav className="navbar navbar-expand-sm">
+        <div className="container">
+          <a className="navbar-brand" href="/">YouTube</a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <i className="icon ion-md-menu menu-icon"></i>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <div className="nav-left col-3 d-none d-sm-block">
+              <ul className="navbar-nav">
+                <li className="nav-item">
+                  <a className="nav-link" href="/">Home</a>
+                </li>
+              </ul>
+            </div>
+            <div className="nav-right col-7 d-none d-sm-block">
+              <SearchBar />
+            </div>
+            <div className="mobile-view d-block d-sm-none">
+              <ul className="navbar-nav">
+                <li className="nav-item ml-auto">
+                  <a className="nav-link" href="/">Home</a>
+                  <a className="nav-link" href="/">Channels</a>
+                </li>
+              </ul>
+              <SearchBar />
+            </div>
           </div>
         </div>
-      </div>
-    </nav>
-  );
-};
+      </nav>
+    );
+  };
+}
 
 export default Navbar;
